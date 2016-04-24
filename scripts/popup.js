dca411bcf59b2a9e92b52a19ce0400efe2b1ddc7
Haberling <1973266@linux09.mcs.sdsmt.edu>
2016-04-24 17:20:02 -0600

    Some of coments

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,4 +1,12 @@
+/*
+Description:
+This function makes a request to get_course_info.php to get course info so it can
+be set as the content of the modal popup. It then makes the popup visible.
 
+Arguments:
+  prefix: the prefix of the course to display
+  number: string of the course number or numbers
+*/
 function openPopup(prefix, number) {
     requestUrl = "get_course_info.php?preFix="+prefix+"&number="+number;
     //requestUrl = "get_course_info.php?preFix=CSC&number=300";
@@ -13,7 +21,10 @@ function openPopup(prefix, number) {
     modal.style.display = "block";
 }
 
-
+/*
+Description:
+This function sets the 
+*/
 function changeData(data)
 {
   document.getElementById('modalContent').innerHTML = data;
